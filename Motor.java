605944a02d69c3bf5e14a0ab55ebb056309c9432
melizahaiziK <melizaha1zik@gmail.com>
2026-09-08 22:23:48 +0700

    modifikasi

--- Motor.java
+++ Motor.java
@@ -24,10 +24,14 @@ public class Motor {
     }
 
     public void setKecepatan(int kecepatan) {
-       if (!this.isMesinOn && kecepatan > 0) {
+       if (kecepatan < 0) {
+        System.out.println("Kecepatan tidak boleh negatif");
+       } else if (!this.isMesinOn && kecepatan > 0) {
         System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+       } else if (kecepatan > 100){
+            System.out.println("Kecepatan maksimal adalah 100");
        } else {
-            this.kecepatan = kecepatan;
+        this.kecepatan = kecepatan;
        }
     }
 
